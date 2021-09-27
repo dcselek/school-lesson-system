c86fbd6bcddef8216d74a9acedd974683126dd97
Doğan Can Selek <canselek2434@gmail.com>
2021-09-28 01:20:45 +0300

    add story for buttons

--- src/stories/StyledButton.js
+++ src/stories/StyledButton.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Button as CarbonButton} from 'carbon-components-react';
+import { Button as CarbonButton } from 'carbon-components-react';
 
 const Button = styled(CarbonButton)`
         border: 2px solid red
     `;
 
-export default function StyledButton() {
+export default function StyledButton({ label, ...props }) {
     return (
         <div>
-            <Button>Styled Button</Button>
+            <Button {...props}> {label} </Button>
         </div>
     )
 }
