7de353fa5073c8003b1e3a4ed29b94e99b41c015
Doğan Can Selek <canselek2434@gmail.com>
2021-09-28 01:37:06 +0300

    remove props

--- src/stories/CarbonButton.jsx
+++ src/stories/CarbonButton.jsx
@@ -5,11 +5,9 @@ import { Button } from 'carbon-components-react';
 /**
  * Primary UI component for user interaction
  */
-export const CarbonButton = ({ kind, size, label, ...props }) => {
+export const CarbonButton = ({ label, ...props }) => {
   return (
     <Button
-      kind={kind}
-      size={size}
       {...props}
     >
       {label}
