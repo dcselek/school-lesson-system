9a361f94e4f1584245350751db5a13eb88648325
Doğan Can Selek <canselek2434@gmail.com>
2021-09-28 15:50:41 +0300

    add header components and story

---
+++ src/stories/Header.stories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../components/header/Header';
+
+export default {
+    title : "Components/Header",
+    argTypes: {
+        login : {
+            control: {type: "boolean"}
+        }
+    }
+};
+
+const TemplateHeader = (args) => <Header {...args} />
+
+export const HeaderBase = TemplateHeader.bind({});
