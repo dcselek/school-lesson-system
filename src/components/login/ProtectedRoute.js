d6069cba59721ac8c48a53b69d0a1167ce4ca440
Doğan Can Selek <canselek2434@gmail.com>
2021-10-01 13:44:29 +0300

    update ProtectedRoute.js

--- src/components/login/ProtectedRoute.js
+++ src/components/login/ProtectedRoute.js
@@ -4,7 +4,7 @@ import useAuth from '../../context/AuthContext'
 
 export default function ProtectedRoute({ element, path }) {
     const { authed } = useAuth();
-    const ele = authed === true ? element : <Navigate to="/main" replace />
+    const ele = authed === true ? element : <Navigate to="/main" replace state={{ path }} />
     return (
         <Route path={path} element={ele} />
     )
