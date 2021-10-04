f435b8a31ef1661018a565285de300b7441aeb9b
Doğan Can Selek <canselek2434@gmail.com>
2021-10-04 17:42:47 +0300

    add fake logout

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import MainButton from '../components/buttons/MainButton';
 import useAuth from '../context/AuthContext'
 
 export default function MainPage() {
 
+    const [data, setData] = useState(null)
     const { logout } = useAuth();
     const navigate = useNavigate();
 
-    const handleLogout = () => {
-        logout();
-        navigate("/");
+
+    async function handleLogout() {
+
+        const response = await fetch("/me", {
+            method: "GET", headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const status = await response.status
+
+        if (status === 403) {
+            logout();
+            navigate("/");
+        }
     }
+
+
+    useEffect(() => {
+        async function authMe() {
+            const response = await fetch("/me", {
+                method: "GET", headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer 123'
+                }
+            })
+
+            const data = await response.json();
+
+            setData(data.username);
+        }
+
+        authMe();
+    }, [])
 
     return (
         <div>
-            <h1>Main Page</h1>
-            <MainButton onClick={handleLogout} kind="secondary" label="Logout" />
+            <h1>Main Page Hoşgeldin {JSON.stringify(data)}</h1>
+            <MainButton onClick={handleLogout} kind="secondary" label="Fake Logout" />
         </div>
     )
 }
