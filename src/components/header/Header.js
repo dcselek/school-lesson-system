c39436133bc5ed8fd16d744b493c0b66d23c2f3e
Doğan Can Selek <canselek2434@gmail.com>
2021-10-06 18:19:19 +0300

    add HeaderContainer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { Header as HeaderBase, HeaderName, HeaderNavigation, HeaderMenuItem } from 'carbon-components-react'
+import { Header as HeaderBase, HeaderName, HeaderNavigation, HeaderMenuItem, HeaderContainer } from 'carbon-components-react'
 
 export default function Header({ login }) {
     return (
-        <HeaderBase>
-            <HeaderName href="#" prefix="DCS">
-                School System
-            </HeaderName>
-            {login &&
-                <HeaderNavigation>
-                    <HeaderMenuItem>Main</HeaderMenuItem>
-                    <HeaderMenuItem>Lessons</HeaderMenuItem>
-                </HeaderNavigation>
-            }
-        </HeaderBase>
+        <HeaderContainer render={() => (
+            <HeaderBase>
+                <HeaderName href="#" prefix="DCS">
+                    School System
+                </HeaderName>
+                {login &&
+                    <HeaderNavigation>
+                        <HeaderMenuItem>Profile</HeaderMenuItem>
+                        <HeaderMenuItem>Lessons</HeaderMenuItem>
+                        <HeaderMenuItem>Students List</HeaderMenuItem>
+                    </HeaderNavigation>
+                }
+            </HeaderBase>
+        )} />
     )
 }
