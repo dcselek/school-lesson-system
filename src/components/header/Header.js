a117f29fda7c23deceada40a1a2ca36703c899b5
Doğan Can Selek <canselek2434@gmail.com>
2021-10-07 16:33:09 +0300

    add logout button in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,48 @@
 import React from 'react'
-import { Header as HeaderBase, HeaderName, HeaderNavigation, HeaderMenuItem, HeaderContainer } from 'carbon-components-react'
+import { Header as HeaderBase, HeaderName, HeaderNavigation, HeaderMenuItem, HeaderContainer, HeaderGlobalBar, HeaderGlobalAction } from 'carbon-components-react'
+import useAuth from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom';
+import { Logout32 } from '@carbon/icons-react';
 
-export default function Header({ login }) {
+export default function Header() {
+    const { logout, authed } = useAuth();
+    const navigate = useNavigate();
+
+    async function handleLogout() {
+
+        const response = await fetch("/me", {
+            method: "GET", headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const status = await response.status
+
+        if (status === 403) {
+            logout();
+            navigate("/");
+        }
+    }
+
     return (
         <HeaderContainer render={() => (
             <HeaderBase>
                 <HeaderName href="#" prefix="DCS">
                     School System
                 </HeaderName>
-                {login &&
-                    <HeaderNavigation>
-                        <HeaderMenuItem>Profile</HeaderMenuItem>
-                        <HeaderMenuItem>Lessons</HeaderMenuItem>
-                        <HeaderMenuItem>Students List</HeaderMenuItem>
-                    </HeaderNavigation>
+                {authed &&
+                    <>
+                        <HeaderNavigation>
+                            <HeaderMenuItem>Profile</HeaderMenuItem>
+                            <HeaderMenuItem>Lessons</HeaderMenuItem>
+                            <HeaderMenuItem>Students List</HeaderMenuItem>
+                        </HeaderNavigation>
+                        <HeaderGlobalBar>
+                            <HeaderGlobalAction aria-label="logout" onClick={handleLogout} >
+                                <Logout32 />
+                            </HeaderGlobalAction>
+                        </HeaderGlobalBar>
+                    </>
                 }
             </HeaderBase>
         )} />
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,29 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import MainButton from '../components/buttons/MainButton';
-import useAuth from '../context/AuthContext'
 
 export default function MainPage() {
 
     const [data, setData] = useState(null)
-    const { logout } = useAuth();
-    const navigate = useNavigate();
-
-    async function handleLogout() {
-
-        const response = await fetch("/me", {
-            method: "GET", headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const status = await response.status
-
-        if (status === 403) {
-            logout();
-            navigate("/");
-        }
-    }
 
     useEffect(() => {
         async function authMe() {
@@ -45,7 +24,6 @@ export default function MainPage() {
     return (
         <>
             <h1>Main Page Hoşgeldin {JSON.stringify(data)}</h1>
-            <MainButton onClick={handleLogout} kind="secondary" label="Fake Logout" />
         </>
     )
 }
