fd399141d4975bd78f5635057ef62a3a19593d80
Doğan Can Selek <canselek2434@gmail.com>
2021-10-15 11:38:00 +0300

    add LessonListPage

---
+++ src/pages/LessonsListPage.js
@@ -0,0 +1,14 @@
+import { Grid,Row } from 'carbon-components-react'
+import React from 'react'
+
+export default function LessonsListPage() {
+    return (
+        <>
+          <Grid>
+              <Row>
+                
+              </Row>
+          </Grid>  
+        </>
+    )
+}
