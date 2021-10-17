691b70c0e53b44a5d3e72f9582552344f446b1db
Doğan Can Selek <canselek2434@gmail.com>
2021-10-17 16:04:49 +0300

    and lessons endpoint

--- src/mocks/api/index.js
+++ src/mocks/api/index.js
@@ -24,5 +24,37 @@ export const handlers = [
     return res(
       ctx.status(403)
     )
+  }),
+  rest.get("/lessons", (req, res, ctx) => {
+    return res(
+      ctx.json(
+        [
+          {
+            id: 1,
+            lessonCode: "BTO331",
+            name: "Bilgisayar Ağları",
+            value: 43,
+          },
+          {
+            id: 2,
+            lessonCode: "BTO330",
+            name: "Bilgisayar Donanımları",
+            value: 15,
+          },
+          {
+            id: 3,
+            lessonCode: "BTO365",
+            name: "Veri Güvenliği",
+            value: 43,
+          },
+          {
+            id: 4,
+            lessonCode: "BTO355",
+            name: "Web Tabanlı Programlama",
+            value: 21,
+          }
+        ]
+      )
+    )
   })
 ]
