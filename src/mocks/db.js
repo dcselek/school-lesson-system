9a12d23b3246d9f5d1090e5b93df3c057826d5af
Doğan Can Selek <canselek2434@gmail.com>
2021-10-21 22:34:38 +0300

    add db

---
+++ src/mocks/db.js
@@ -0,0 +1,13 @@
+import { name, datatype } from 'faker'
+import { factory, primaryKey } from '@mswjs/data'
+
+
+
+export const db = factory({
+    user: {
+        id: primaryKey(datatype.uuid),
+        firstName: name.firstName,
+        // firstName: name.firstName,
+        // lastName: name.lastName
+    }
+})
