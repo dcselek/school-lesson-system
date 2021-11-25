c1e64c4cbb94c38701cb9d23a2c39a130ad09e0e
Doğan Can Selek <canselek2434@gmail.com>
2021-11-25 14:00:18 +0300

    isolate login

--- src/stories/Login Page/Login.stories.js
+++ src/stories/Login Page/Login.stories.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import LoginForm from '../../components/login/LoginForm'
+import { AuthProvider } from '../../context/AuthContext'
 
 export default {
-    title: "Pages/Login Page/Components"
+    title: "Components/LoginForm"
 }
 
-const TemplateLoginForm = (args) => <LoginForm {...args} />
+const TemplateLoginForm = (args) => <Router><AuthProvider><LoginForm {...args} /></AuthProvider></Router>
 
 export const Login = TemplateLoginForm.bind({});
--- src/stories/Login Page/LoginPage.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import LoginPage from '../../pages/loginPage'
-
-export default {
-    title: "Pages/Login Page"
-}
-
-export const PageLogin = () => <LoginPage />
